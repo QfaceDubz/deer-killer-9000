58685684a771208b1ef02f5e9362985666483608
QfaceDubz <quacface2@gmail.com>
2024-07-09 16:02:00 -0400

    Deer collisions and things

---
+++ Assets/Scripts/Deer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deer : MonoBehaviour {
+    bool touchingTree;
+    float speed = 900;
+    public int strafeSide; // left or right, but it's a bool
+    Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        strafeSide = Random.Range(0, 2);
+    }
+
+    void Update() {
+        // move deer forward (set velocity)
+        rb.velocity = transform.up * Time.deltaTime * speed;
+
+        // move deer to one side if touching a tree
+        if (touchingTree && strafeSide == 0)
+            rb.velocity = -transform.right * Time.deltaTime * speed;
+
+        if (touchingTree && strafeSide == 1)
+            rb.velocity = transform.right * Time.deltaTime * speed;
+
+        // if past certain z coord, remove deer
+        if (transform.position.z < -30)
+            Destroy(gameObject);
+
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (col.gameObject.tag == "tree") {
+            touchingTree = true;
+            strafeSide = Random.Range(0, 2);
+        }
+    }
+
+    void OnCollisionExit(Collision col) {
+        touchingTree = false;
+    }
+
+
+}
